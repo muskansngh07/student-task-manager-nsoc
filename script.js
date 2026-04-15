60c6ee1553b09cf942992d4f6f93239bcb343c4a
priyakalamkar555-art <priyakalamkar555@gmail.com>
2026-04-15 16:34:35 +0530

    Fix: add validation for empty task input

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function addTask() {
   const input = document.getElementById("taskInput");
   const task = input.value;
+  const errorMsg = document.getElementById("errorMsg");
 
-  if (task === "") return;
+  if (task.trim()=== "") {
+    errorMsg.textContent = " Please enter a task.";
+    return;
+  };
 
   const li = document.createElement("li");
   li.innerHTML = `${task} <button onclick="this.parentElement.remove()">❌</button>`;
