fc0c7a62f246de01fc633f6e2286b6b0f88cda9d
priyakalamkar555-art <priyakalamkar555@gmail.com>
2026-04-16 15:57:14 +0530

    Add edit button for tasks

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 function addTask() {
   const input = document.getElementById("taskInput");
-  const task = input.value;
+  const task = input.value.trim();
   const errorMsg = document.getElementById("errorMsg");
 
   if (task.trim()=== "") {
     errorMsg.textContent = " Please enter a task.";
     return;
   };
+  errorMsg.textContent = "";
 
   const li = document.createElement("li");
-  li.innerHTML = `${task} <button onclick="this.parentElement.remove()">❌</button>`;
-
+  const span = document.createElement("span");
+  span.textContent = task;
+  const editButton = document.createElement("button");
+    editButton.textContent = "Edit";
+    editButton.addEventListener("click", function () {
+      const newTask = prompt("Edit task:", span.textContent);
+      if (newTask !== null) {
+        span.textContent = newTask;
+      }
+    });
+    li.appendChild(span);
+    li.appendChild(editButton);
+  const removeButton = document.createElement("button");
+  removeButton.textContent = "Remove";
+  removeButton.addEventListener("click", function () {
+    li.remove();
+  });
+  li.appendChild(removeButton);
   document.getElementById("taskList").appendChild(li);
   
   input.value = "";
 }
-
