bbb4181682e9d4f3523e037ecf9156ab3f0c80a1
priyakalamkar555-art <priyakalamkar555@gmail.com>
2026-04-18 00:54:55 +0530

    Upgraded Project with features of real-time Day,Date & Time

--- script.js
+++ script.js
@@ -8,10 +8,19 @@ function addTask() {
     return;
   };
   errorMsg.textContent = "";
-
   const li = document.createElement("li");
   const span = document.createElement("span");
   span.textContent = task;
+  const now = new Date();
+  const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const day = dayNames[now.getDay()];
+  const date =`${now.getDate()} ${now.toLocaleString("default", { month: "long" })} ${now.getFullYear()}`;
+  const time = now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+  const timeElement = document.createElement("small");
+  timeElement.textContent = ` (${day}, ${date} at ${time})`;
+  timeElement.style.marginLeft = "10px";
+  timeElement.style.color = "#888";
+
   const editButton = document.createElement("button");
     editButton.textContent = "Edit";
     editButton.addEventListener("click", function () {
@@ -21,6 +30,7 @@ function addTask() {
       }
     });
     li.appendChild(span);
+    li.appendChild(timeElement);
     li.appendChild(editButton);
   const removeButton = document.createElement("button");
   removeButton.textContent = "Remove";
